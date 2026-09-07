db190c87fa7aedc975c16c499309736e242a216a
herediaangie777-coder <herediaangie777@gmail.com>
2026-09-07 06:41:05 -0500

    Clase 7 de septiembre

---
+++ Programacion orientada a objetos(POO)/poo/empresa/desarrollador.py
@@ -0,0 +1,4 @@
+from empresa.empleado import Empleado
+class Desarrollador (Empleado):
+    def calcular_bonificacion(self):
+        return self.salario*0.15
---
+++ Programacion orientada a objetos(POO)/poo/empresa/gerente.py
@@ -0,0 +1,4 @@
+from empresa.empleado import Empleado
+class Gerente (Empleado):
+    def calcular_bonificacion(self):
+        return self.salario*0.20
